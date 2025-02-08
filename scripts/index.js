2e877fca96115d258c4ddb6b160f2c187d0b2675
bongiBong-create <mairanss543@inbox.ru>
2025-02-08 23:49:29 +0300

    preloader

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,20 @@
 const slider = document.querySelector(".slider-container");
 const slide = document.querySelector(".place-slide");
 const pagination = document.querySelector(".pagination");
+const preloader = document.querySelector(".preloader");
+const content = document.querySelector(".main");
 
 let currentSLide = 1;
 const slideWidth = slide.clientWidth;
 pagination.textContent = "Рис. 1/3";
+
+const loader = () => {
+  setTimeout(() => {
+    preloader.style.display = "none";
+    preloader.style.opacity = "0";
+    content.style.opacity = "1";
+  }, 2000);
+};
 
 const move = (e) => {
   console.log(e.target.alt);
@@ -19,4 +29,5 @@ const move = (e) => {
   pagination.textContent = `Рис. ${currentSLide}/3`;
 };
 
+window.addEventListener("load", loader);
 slider.addEventListener("mouseup", move);
